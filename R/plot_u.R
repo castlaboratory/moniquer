9d3cff0ac0d547356409054dc8d28d4b194c3750
gabrielteotonio <ghft1@de.ufpe.br>
2021-11-15 11:26:52 -0300

    feat(plot_u): add phases argument

--- R/plot_u.R
+++ R/plot_u.R
@@ -7,13 +7,18 @@
 #' @param n Variable for sample size (opportunity area)
 #' @param c Variable for number of occurrences
 #' @param k Variable for sample index
+#' @param phase Value to indicate phases range
 #' @param data Data set with the variables n, d, and k
 #' @return A Plotly graphic
 #' @export
-plot_u <- function(n, c, k, data) {
+plot_u <- function(n, c, k, phase, data) {
   # Filters -----
   if (dim(data)[1] == 0)  {
     stop("data must be a non-empty dataframe.")
+  }
+
+  if (is.na(phase)) {
+    stop("phase must be a valid index for data observations.")
   }
 
   # if (!(n %in% colnames(data)) || !(d %in% colnames(data)) || !(k %in% colnames(data))) {
@@ -22,19 +27,26 @@ plot_u <- function(n, c, k, data) {
 
   # Calculation -----
   data$k <- data[[k]]
-  data$u <- 100 * (data[[c]]/data[[n]])
-  data$u_bar <- 100 * (sum(data[[c]])/sum(data[[n]]))
+  data_phase_1 <- data[1:phase, ]
+  data_phase_2 <- data[phase:nrow(data), ]
 
-  data$ucl <- data$u_bar + (3 * (sqrt(data$u_bar/data[[n]])))
-  data$lcl <- data$u_bar - (3 * (sqrt(data$u_bar/data[[n]])))
+  data_phase_1$u <- 100 * (data_phase_1[[c]]/data_phase_1[[n]])
+  data_phase_2$u <- 100 * (data_phase_2[[c]]/data_phase_2[[n]])
+  data_phase_1$u_bar <- 100 * (sum(data_phase_1[[c]])/sum(data_phase_1[[n]]))
+
+  data_phase_1$ucl <- data_phase_1$u_bar + (3 * (sqrt(data_phase_1$u_bar/data_phase_1[[n]])))
+  data_phase_1$lcl <- data_phase_1$u_bar - (3 * (sqrt(data_phase_1$u_bar/data_phase_1[[n]])))
 
   # Build graphic -----
-  data %>%
+  data_phase_1 %>%
     plot_ly(x = ~k,
             y = ~u,
             mode = "lines",
             source = "plot_u") %>%
-    add_trace(y = ~u, name = 'Contagem por unidade', mode = 'lines+markers') %>%
+    add_trace(y = ~u,
+              name = 'Contagem por unidade',
+              mode = 'lines+markers',
+              color = I("blue")) %>%
     add_trace(y = ~u_bar,
               name = TeX("\\bar{u}"),
               type = 'scatter',
@@ -52,10 +64,17 @@ plot_u <- function(n, c, k, data) {
               mode = 'lines',
               color = I("red"),
               line = list(shape = "hv")) %>%
-    layout(title = paste0("Gráfico U de ", names(data[c])),
+    layout(title = paste0("Gráfico U de ", names(data_phase_1[c])),
            xaxis = list(title = 'Amostra'),
            yaxis = list(title = 'Contagem por unidade'),
-           hovermode = "x unified") %>%
+           hovermode = "x unified",
+           showlegend = FALSE) %>%
+    add_trace(data = data_phase_2,
+              name = "Contagem por unidade",
+              x = ~k,
+              y = ~u,
+              color = I("blue"),
+              mode = 'lines+markers') %>%
     config(modeBarButtonsToRemove = c("zoomIn2d", "zoomOut2d",  'zoom2d', 'pan2d', 'resetScale2d',
                                       'hoverClosestCartesian', 'hoverCompareCartesian',
                                       'select2d','lasso2d', 'toggleSpikelines'),
